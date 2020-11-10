e7312508355b36b17f89befa6a0d60601a38a2c8
Hércules HRsniper <63937242+HRsniper@users.noreply.github.com>
2020-11-09 23:02:31 -0400

    add styled

---
+++ web/src/pages/Login/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { Container } from "./styles";
+
+export const Login = (): JSX.Element => {
+  return (
+    <Container>
+      <h1>Login</h1>
+    </Container>
+  );
+};
